8dbca0d4b6306b64add92d8956445b5fa643bcd2
Miguel <miguel.n.pena@gmail.com>
2026-06-28 15:02:40 -0400

    Adding test page to rearrange

--- rearrange_test.py
+++ rearrange_test.py
@@ -1,1 +1,7 @@
+# ---- Tests ----
+print(rearrange_name("Turing, Alan"))        # Expected: Alan Turing
+print(rearrange_name("Curie, Marie"))        # Expected: Marie Curie
+print(rearrange_name("Einstein, Albert"))    # Expected: Albert Einstein
 
+# Edge case: no coma
+print(rearrange_name("Ada Lovelace"))        # Expected: Ada Lovelace
