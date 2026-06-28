fef2ee8b4b181e4c8b50fdae90eabaf315f68b81
Miguel <miguel.n.pena@gmail.com>
2026-06-28 14:54:22 -0400

    Adding the rearrange code to the .py file

--- rearrange.py
+++ rearrange.py
@@ -1,1 +1,9 @@
+# code for rearrange, Blue-Kale didn't have it setup in the Repository.
 
+def rearrange_name(name):
+    # If unsure about format, we check for a coma
+    if "," not in name:
+        return name          # No rearranging needed
+
+    last, first = name.split(",")
+    return f"{first.strip()} {last.strip()}"
